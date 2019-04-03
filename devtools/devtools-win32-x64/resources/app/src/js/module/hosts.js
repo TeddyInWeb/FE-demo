d1dd0877285fd0a935b8d9ce085bd679bbb55436
张熙弘 <zhangxihong@dstcar.com>
2019-04-03 10:49:38 +0800

    更新

--- devtools/devtools-win32-x64/resources/app/src/js/module/hosts.js
+++ devtools/devtools-win32-x64/resources/app/src/js/module/hosts.js
@@ -1,4 +1,3 @@
-
 /**
  * 修改hosts模块
  */
@@ -7,11 +6,13 @@ const Fs = require('fs');
 const Util = require('../util.js');
 const filePath = 'C:/Windows/System32/drivers/etc/hosts';
 
-function readHosts(){
+let saving = false;
+
+function readHosts() {
     Fs.readFile(filePath, (err, data) => {
-        if(err){
+        if (err) {
             return void alert('出错啦! 请重试~');
-        }else{
+        } else {
             let dataStr = data.toString('utf-8');
             document.getElementById('hosts-edit-screen').value = dataStr;
             Util.openPop('tool-hosts-pop');
@@ -19,18 +20,25 @@ function readHosts(){
     })
 }
 
-function saveFile(){
+function saveFile() {
+
+    if (saving) {
+        return;
+    }
+    saving = true;
+
     let dataStr = document.getElementById('hosts-edit-screen').value;
     Fs.writeFile(filePath, dataStr, (err) => {
-        if(err){
-            alert('保存失败!');
-        }else{
+        saving = false;
+        if (err) {
+            alert('保存失败: ' + err);
+        } else {
             alert('保存成功!');
         }
     });
 }
 
-function isHostExist(dataStr, host31Ip, host31Name){
+function isHostExist(dataStr, host31Ip, host31Name) {
     let matcher = new RegExp('\\#*' + host31Ip + '\\s+' + host31Name, 'g');
     return dataStr.match(matcher);
 }
